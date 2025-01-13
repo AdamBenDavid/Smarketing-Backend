7c09ea5c9320cf711e4cc783b423a5f13af1cbc3
User <user@gmail.com>
2025-01-13 19:23:08 +0200

    init: initialize project from base assignment

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`); 
 });
