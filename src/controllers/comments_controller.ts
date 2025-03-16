68c74bb3325f6fda9c40121caa834d1aff009904
Aviv <avivush2001@cs.colman.ac.il>
2025-03-16 16:30:16 +0200

    fix it

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -27,7 +27,9 @@ const addComment = async (req: Request, res: Response) => {
     const comment = new commentsModel({
       userId: user._id,
       fullName: user.fullName,
-      profilePicture: user.profilePicture || "https://i.pravatar.cc/50",
+      profilePicture:
+        user.profilePicture ||
+        "http://localhost:3000/images/default-profile.png",
       commentData,
       postId,
     });
